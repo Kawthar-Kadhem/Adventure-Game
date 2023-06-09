7de39ea7c7807ed72ab92b1780dd137d8e3d372a
Kawthar-Kadhem <134386586+Kawthar-Kadhem@users.noreply.github.com>
2023-06-09 23:44:14 +0300

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -13,7 +13,7 @@ def intro(rumor, option):
     print_pause("The neighborhood you are standing at has " + rumor + " around "
                 "as for that there is no much people at this time 8:00 PM \n")
     print_pause("There is somone wearing red coat just across the street.\n")
-    print_pause("you tride to see the person face but he/she is giving you his/her back \n")
+    print_pause("you tride to see the person face but she is giving you his/her back \n")
 
 
 def move(rumor, option):
@@ -24,42 +24,37 @@ def move(rumor, option):
                     )
         print_pause("\nThe bus has arrived and you took it to go home.\n")
     else:
-        print_pause('\nAlthough you descide to stay in your place but you got curiouse so you decided to walk out of the station'
-                    "\nwhile you were walking, the person took steps further from you"
+        print_pause('\nthough you descide to stay in your place, you got curiouse so you decided to walk'
+                    "\nwhile you were walking, the person took steps further"
                     '\nyou stopped once you noticed the move '
                     "\nYou heard a soft voice whispering"
                     "\nDon't try to come near, it is not good to you"
-                    "\nSo you went back to the station in fear\n" )
-        
+                    "\nSo you went back to the station in fear\n")
         play_again()
 
 
 def check(rumor, option):
     print_pause("\nYou went close to the person to check.")
     print_pause("\nWhile you were walking it started to" + option + "")
-    print_pause("\nThe " + option + " slowed your walking as it started heavy\n")
+    print_pause("\nThe " + option + " slowed your walking \n")
     while True:
-        choice2 = input("(Would you like to (s) shout out or (t)  turn back?)\n")
+        choice2 = input("(Would you like to (s)shout or (t)turn back?)\n")
         if choice2 == "s":
             print_pause("\nwhile it kept " + option + " you shouted out loud HELLO! "
-                        "do you need any help?")
+                        "do you need help?")
             print_pause("\nThe person turned out to you "
-                        "without talking" )
+                        "without talking")
             print_pause("\nBut the " + option + "slowly become lighter"
                         "so you kept on moving toward him/her")
             print_pause("\nYou reach where he'she was standing but " + option +
-                        ". started to pour heavy again that your scream wasn't heard at all\n")
-            break
-                
+                        ".started to pour heavy that your scream wasn't heard\n")
         elif choice2 == "t": 
             print_pause("\nYou tride to shout before turning back")
-            print_pause("\nbut your your voice wasn't hearable beacuse of" + option + ".")
+            print_pause("\nbut your voice wasn't hearable beacuse of"+ option +".")
             print_pause("\nYou Regret it later as you find out just before the bus move it is a pregnant women that was in pain")
             play_again()
-            play_game()
 
-
-def scenario(rumor, option):
+def scen(rumor, option):
     print_pause("Statemnt 1 to check")
     print_pause("Statemnt 2 to wait for the bus")
     print_pause("What is your decision?")
@@ -88,7 +83,7 @@ def play_game():
     rumor = "Ghost"
     option = random.choice(["Snowing", "Raining"])
     intro(rumor, option)
-    scenario(rumor, option)
+    scen(rumor, option)
 
 
 play_game()
